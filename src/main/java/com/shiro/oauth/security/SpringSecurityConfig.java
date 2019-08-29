4186c57613e2ace608fbc55344873200b6ef608e
Albano Yanes <ajyanreyu@gmail.com>
2019-08-29 10:29:52 +0100

    Spring security configuration

---
+++ src/main/java/com/shiro/oauth/security/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.shiro.oauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userService;
+
+    /**
+     * Register the user services in the authentication manager
+     *
+     * @param auth AuthenticationManagerBuilder
+     * @throws Exception
+     */
+    @Override
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
+    }
+
+    /**
+     * Encrypt passwords to BCrypt
+     *
+     * @return BCryptPasswordEncoder
+     * @author Albano yanes <ajyanreyu@gmail.com>
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+
+    }
+}
